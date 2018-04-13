c1b4db6676a0077ad4427483dc229ee0fc601ee5
DracotMolver <dracotm25@gmail.com>
2018-04-12 22:49:42 -0300

    testing the extension

--- test/extension.test.js
+++ test/extension.test.js
@@ -1,24 +1,80 @@
-/* global suite, test */
-
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
+const {
+    workspace,
+    commands,
+    window
+} = require('vscode');
 const assert = require('assert');
+const sinon = require('sinon');
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
-// const vscode = require('vscode');
-// const myExtension = require('../extension');
+// -================= // =================-
+const units = require('./../helpers/units');
+const {
+    PLACE_HOLDER_INPUT
+} = require('./../constants/strings');
 
-// Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", function() {
+describe("Extension Tests", function () {
 
-    // Defines a Mocha unit test
-    test("Something 1", function() {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    it("Executes the commmand extension", function (done) {
+        commands.executeCommand('extension.unitConverter')
+            .then(resolve => {
+                assert(true);
+                done();
+            }, reject => {
+                done();
+            });
     });
+
+    describe("Input prompt actions", function () {
+        it("Selects `px` from the input prompt", function (done) {
+            const selections = window.activeTextEditor.selections;
+            const start = selections[0].start;
+            const end = selections[0].end;
+
+            if (start.line === end.line || start.character === end.character) {
+                const showQuickPick = sinon.stub(window, 'showQuickPick');
+
+                showQuickPick.resolves({
+                    label: units[0].label,
+                    description: units[0].description
+                });
+
+                window.showQuickPick(units, {
+                    matchOnDescription: true,
+                    placeHolder: PLACE_HOLDER_INPUT
+                }).then(resolve => {
+                    assert(resolve.label === 'px');
+                    assert(resolve.description === 'Pixels');
+                    assert(showQuickPick.calledOnce);
+                    done();
+                });
+            }
+        });
+    });
+
+
+    // // It displays the input prompt to type the units manually
+    // window.showQuickPick(units, {
+
+    // }).then(resolve => {
+    //     if (resolve) {
+    //         // Displays the input prompt to get the values
+    //         window.showInputBox({
+    //             promptt: PLACE_HOLDER_PROMPTt,
+    //             value: ''
+    //         }).then(resolve => {
+    //             if (resolve) {
+    //                 // Removes the units in case it was entered
+    //                 // Converter.value = resolve.toLowerCase().trim().replace(/rem|px|em|#|rgb/ig, '');
+    //                 // Converter.setType(resolve.label.toLowerCase());
+    //                 window.showInformationMessage(CONVERTED_VALUE);
+    //             }
+    //         }, reject => {
+    //             window.showErrorMessage(ERROR_PROMPTt);
+    //         });
+    //     }
+    // }, reject => {
+    //     window.showErrorMessage(ERROR_INPUT);
+    // })
+
+
 });
