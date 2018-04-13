4af8972286bfe0373d1a90b871c48fc73ab23bb4
DracotMolver <dracotm25@gmail.com>
2018-04-12 22:49:56 -0300

    adding constants

---
+++ constants/strings.js
@@ -0,0 +1,9 @@
+module.exports = {
+    PLACE_HOLDER_INPUT: 'Convert unit from:',
+    PLACE_HOLDER_PROMPT: 'Set the value to convert:',
+    CONVERTED_VALUE: 'The converted value is: $s',
+
+    // Error messages
+    ERROR_INPUT: 'There was an error trying to execute this action',
+    ERROR_PROMPT: 'There was an error trying to convert a unit'
+};
