8f6e7253a0bb93e4f419f12b80e3a3efbdc5e135
DracotMolver <dracotm25@gmail.com>
2018-04-12 22:50:04 -0300

    move to its own file

---
+++ helpers/units/index.js
@@ -0,0 +1,8 @@
+module.exports = [
+    { label: 'px', description: 'Pixels' },
+    { label: '[em|rem]', description: 'M' },
+    { label: '#', description: 'Hexadecimal' },
+    { label: 'rgb', description: 'Red Green Blue' },
+    { label: 'rgba', description: 'Red Green Blue Alpha' },
+    { label: 'color', description: 'Color value (e.g. white)' }
+];
